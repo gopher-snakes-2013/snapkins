2847add1472c688777d1759225b33e9a2c503edf
bbeaird <bbeaird@gmail.com>
2013-10-14 18:16:00 -0700

    end of timebox 10-14-2013, 18:15

--- public/doodler.js
+++ public/doodler.js
@@ -1,1 +1,10 @@
+var homepage_canvas = document.getElementById("doodleView")
 
+function fillRect() {
+  homepage_canvas = homepage_canvas.getContext("2d");
+  homepage_canvas.fillRect(50,25,150,100);
+}
+
+function clearCanvas(){
+  homepage_canvas.width = homepage_canvas.width;
+}
