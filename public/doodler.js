0389dbc56a4c53a073fbdf75801c61658d7dcfc8
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-10-15 16:37:00 -0700

    Demonstrated sort-of drawing, LIKE A BOSS

--- public/doodler.js
+++ public/doodler.js
@@ -1,3 +1,18 @@
-// our_canvas = document.canvas
-our_canvas = $('canvas');
+var canvas = document.getElementById('canvas')
+var context = canvas.getContext('2d')
 
+
+canvas.addEventListener("mousemove", draw, false)
+
+function draw(event){
+  console.log(event);
+  var x = event.x;
+  var y = event.y;
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+  context.beginPath();
+  context.moveTo(x,y);
+  context.strokeStyle = "magenta"
+  context.lineTo(x+500, y);
+  context.stroke();
+}
