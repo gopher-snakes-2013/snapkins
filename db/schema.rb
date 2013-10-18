19c35e2036a71e23aaab5ef67f85ca02e70f8a55
Jenny Yee <jennyyee@Jennycat.local>
2013-10-17 20:16:05 -0700

    too late

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017001959) do
+ActiveRecord::Schema.define(version: 20131018020059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "snapkins", force: true do |t|
-    t.string   "image_url"
+    t.text     "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
